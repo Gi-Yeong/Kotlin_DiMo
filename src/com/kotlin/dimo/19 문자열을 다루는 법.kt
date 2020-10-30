dfe671d32bf2fc547ec41b5d2aa1408ed9bddf8c
kiyoung85 <kiyoung85@gmail.com>
2020-10-30 20:05:08 +0900

    문자열을 다루는 법

---
+++ src/com/kotlin/dimo/19 문자열을 다루는 법.kt
@@ -0,0 +1,18 @@
+package com.kotlin.dimo
+
+fun main() {
+
+    // 일치 하는 경우에만 true 반환
+    var test3 = "kotlin.kt"
+    var test4 = "java.java"
+
+    println(test3.startsWith("java"))
+    println(test4.startsWith("java"))
+
+    println(test3.endsWith(".kt"))
+    println(test4.endsWith(".kt"))
+
+    println(test3.contains("lin"))
+    println(test4.contains("lin"))
+
+}
