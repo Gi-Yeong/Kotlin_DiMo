2443bb959e759f78d7554e77c7874960b209157a
kiyoung85 <kiyoung85@gmail.com>
2020-10-31 19:24:17 +0900

    비트연산

---
+++ src/com/kotlin/dimo/29 비트연산.kt
@@ -0,0 +1,19 @@
+package com.kotlin.dimo
+
+fun main() {
+
+    var bitData: Int = 0b10000
+
+    bitData = bitData or (1 shl 2)
+    println(bitData.toString(2))
+
+    var result = bitData and(1 shl 4)
+    println(result.toString(2))
+
+    println(result shr 4)
+
+    bitData = bitData and((1 shl 4).inv())
+    println(bitData.toString(2))
+
+    println((bitData xor(0b10100)).toString(2))
+}
